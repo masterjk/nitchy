b062db0a26437ad634ffb4464fe0b27c723addc0
Joseph Kiok <joseph@kiok.org>
2021-02-13 15:24:48 -0600

    Initial Commit

---
+++ Nitchy/NitchyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NitchyApp.swift
+//  Nitchy
+//
+//  Created by Joseph Kiok on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct NitchyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
